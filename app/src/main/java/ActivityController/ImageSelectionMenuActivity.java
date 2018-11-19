d68cdca4e553abdcfc33f85cab8778a26d547347
Bhulbert2096 <hulbert.6@wright.edu>
2018-11-19 18:22:54 -0500

    Implemented own image picker
    This is my version of an image picker.  it allows the user to select one
    photo from the gallery and take a picture.  Both of these pictures are
    saved as bitmaps as are the file paths. This will also save the image
    take in the format that the phone will create such as
    month,day,year,hour, and minute.jpg

--- app/src/main/java/ActivityController/ImageSelectionMenuActivity.java
+++ app/src/main/java/ActivityController/ImageSelectionMenuActivity.java
@@ -1,24 +1,41 @@
 package ActivityController;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import CustomViews.BaseView;
 import CustomViews.ImageSelectionMenuView;
 import ImageModel.Image;
 import ImageModel.ImageBundle;
-import br.com.packapps.retropicker.callback.CallbackPicker;
-import br.com.packapps.retropicker.config.Retropicker;
+import test.hulbert.seefood.BuildConfig;
 import test.hulbert.seefood.R;
 
 public class ImageSelectionMenuActivity extends AppCompatActivity implements Controllable {
 
-    private Retropicker retropicker;
     private ImageBundle myImageBundle;
+    private Uri outPutfileUri;
+    private Bitmap mBitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,54 +47,22 @@ public class ImageSelectionMenuActivity extends AppCompatActivity implements Con
     }
 
     public void takePicture(View view) {
-        final Retropicker.Builder builder2 =  new Retropicker.Builder(this)
-                .setTypeAction(Retropicker.CAMERA_PICKER)
-                .setImageName("first_image.jpg")
-                .checkPermission(false);
+        Intent intent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMddyyHHmm");
+        String myDate = simpleDateFormat.format(Calendar.getInstance().getTime());
+        File file = new File(Environment.
+                getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                myDate + ".jpg");
 
-        builder2.enquee(new CallbackPicker() {
-            @Override
-            public void onSuccess(Bitmap bitmap, String s) {
-                Image newImage = new Image();
-                myImageBundle = new ImageBundle();
-                newImage.setBitmap(bitmap);
-                newImage.setsFilePath(s);
-                myImageBundle.getImages().add(newImage);
-            }
+        outPutfileUri = FileProvider.getUriForFile(this,BuildConfig.APPLICATION_ID + ".provider",file);
 
-            @Override
-            public void onFailure(Throwable throwable) {
-                throwable.getStackTrace();
-            }
-        });
 
-        retropicker = builder2.create();
-        retropicker.open();
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, outPutfileUri);
+        startActivityForResult(intent, 1);
     }
 
     public void selectImage(View view) {
-        final Retropicker.Builder builder2 =  new Retropicker.Builder(this)
-                .setTypeAction(Retropicker.GALLERY_PICKER)
-                .checkPermission(false);
 
-        builder2.enquee(new CallbackPicker() {
-            @Override
-            public void onSuccess(Bitmap bitmap, String s) {
-                Image newImage = new Image();
-                myImageBundle = new ImageBundle();
-                newImage.setBitmap(bitmap);
-                newImage.setsFilePath(s);
-                myImageBundle.getImages().add(newImage);
-            }
-
-            @Override
-            public void onFailure(Throwable throwable) {
-                throwable.getStackTrace();
-            }
-        });
-
-        retropicker = builder2.create();
-        retropicker.open();
     }
 
     public void reviewImages(View view) {
@@ -88,5 +73,45 @@ public class ImageSelectionMenuActivity extends AppCompatActivity implements Con
     @Override
     public void updateView() {
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+
+            String uri = outPutfileUri.toString();
+            Log.e("uri-:", uri);
+            Toast.makeText(this, outPutfileUri.toString(), Toast.LENGTH_LONG).show();
+
+            try {
+                mBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), outPutfileUri);
+                Image newImage = new Image();
+                newImage.setBitmap(mBitmap);
+                newImage.setsFilePath(uri);
+                myImageBundle.getImages().add(newImage);
+                //Drawable draw = new BitmapDrawable(getResources(), mBitmap);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        } else if (requestCode == 2 && resultCode == RESULT_OK) {
+            Uri pickedImage = data.getData();
+            // Let's read picked image path using content resolver
+            String[] filePath = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(pickedImage, filePath, null, null, null);
+            cursor.moveToFirst();
+            String imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath, options);
+            Image newImage = new Image();
+            newImage.setBitmap(bitmap);
+            newImage.setsFilePath(imagePath);
+            //we can parse this file name to get the date and time it was taken. may need to rotate image??
+            myImageBundle.getImages().add(newImage);
+        }
     }
 }
--- app/src/main/java/ActivityController/MenuActivity.java
+++ app/src/main/java/ActivityController/MenuActivity.java
@@ -1,24 +1,33 @@
 package ActivityController;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 import CustomViews.BaseView;
 import CustomViews.MenuView;
 import ImageModel.Image;
 import ImageModel.ImageBundle;
-import br.com.packapps.retropicker.callback.CallbackPicker;
-import br.com.packapps.retropicker.config.Retropicker;
 import test.hulbert.seefood.R;
 
 public class MenuActivity extends AppCompatActivity implements Controllable {
 
-    private Retropicker retropicker;
     private ImageBundle myImageBundle;
+    private Uri outPutfileUri;
+    Bitmap mBitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,31 +44,50 @@ public class MenuActivity extends AppCompatActivity implements Controllable {
     }
 
     public void gotoGallery(View view) {
-        final Retropicker.Builder builder2 =  new Retropicker.Builder(this)
-                .setTypeAction(Retropicker.GALLERY_PICKER) //Para abrir a galeria passe Retropicker.GALLERY_PICKER
-                .setImageName("first_image.jpg") //Opicional
-                .checkPermission(false);
-
-        builder2.enquee(new CallbackPicker() {
-            @Override
-            public void onSuccess(Bitmap bitmap, String s) {
-                Image newImage = new Image();
-                myImageBundle = new ImageBundle();
-                newImage.setBitmap(bitmap);
-                newImage.setsFilePath(s);
-                myImageBundle.getImages().add(newImage);
-            }
-
-            @Override
-            public void onFailure(Throwable throwable) {
-                throwable.getStackTrace();
-            }
-        });
-
-        retropicker = builder2.create();
-        retropicker.open();
+        Intent intent = new   Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 2);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == 1 && resultCode==RESULT_OK) {
+
+            String uri = outPutfileUri.toString();
+            Log.e("uri-:", uri);
+            Toast.makeText(this, outPutfileUri.toString(),Toast.LENGTH_LONG).show();
+
+            try {
+                mBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), outPutfileUri);
+                Drawable draw = new BitmapDrawable(getResources(), mBitmap);
+                Image newImage = new Image();
+                newImage.setBitmap(mBitmap);
+                newImage.setsFilePath(uri);
+                myImageBundle.getImages().add(newImage);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+        if(requestCode == 2 && resultCode == RESULT_OK){
+            Uri pickedImage = data.getData();
+            // Let's read picked image path using content resolver
+            String[] filePath = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(pickedImage, filePath, null, null, null);
+            cursor.moveToFirst();
+            String imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath, options);
+            Image newImage = new Image();
+            newImage.setBitmap(bitmap);
+            newImage.setsFilePath(imagePath);
+            //we can parse this file name to get the date and time it was taken. may need to rotate image??
+            myImageBundle.getImages().add(newImage);
+        }
+    }
 
 
     @Override
