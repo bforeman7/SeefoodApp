a1c779e9c8e0662300667c6eb67871bd50de550e
Bhulbert2096 <hulbert.6@wright.edu>
2018-11-18 19:20:21 -0500

    Update MenuView.java

--- app/src/main/java/CustomViews/MenuView.java
+++ app/src/main/java/CustomViews/MenuView.java
@@ -1,16 +1,45 @@
 package CustomViews;
 
+import android.content.Context;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
-public class MenuView implements BaseView {
+import ActivityController.MainMenuActivity;
+import test.hulbert.seefood.R;
+
+public class MenuView extends RelativeLayout implements BaseView {
+
+    private Button btTakePicture;
+    private MainMenuActivity mainMenu;
+    private Button btUploadImage;
+
+    public MenuView(Context context){
+        super(context);
+        init(context);
+    }
 
     @Override
     public View getRootView() {
         return null;
     }
 
-    public void init(){
 
+
+    public void init(Context context){
+        View rootView = inflate(context, R.layout.home_final,this);
+       btTakePicture = rootView.findViewById(R.id.bTakePhoto);
+       btUploadImage = rootView.findViewById(R.id.btUploadImage);
+       mainMenu = new MainMenuActivity();
+    }
+
+    public void takePictureButton(){
+
+        //mainMenu.takePicture();
+    }
+
+    public void uploadPictureButton(){
+      //mainMenu.postImage();
     }
 
 }
